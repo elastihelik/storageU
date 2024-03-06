e0d0dc7c11a6462f123a91ff34b5e65a831a88de
Harold Gwen <162418697+elastihelik@users.noreply.github.com>
2024-03-06 09:14:48 +0200

    Create codemain.js

---
+++ codemain.js
@@ -0,0 +1,28 @@
+this.contentsElement.addEventListener("wheel", (e) => {
+			var left;
+
+			if(e.deltaX){
+				left = this.contentsElement.scrollLeft + e.deltaX;
+
+				this.table.rowManager.scrollHorizontal(left);
+				this.table.columnManager.scrollHorizontal(left);
+			}
+		});
+	}
+	
+		if(data && data.length){
+			
+			row = data[0];
+			
+			for(var key in row){
+				let col = {
+					field:key,
+					title:key,
+				};
+				
+				let value = row[key];
+				
+				switch(typeof value){
+					case "undefined":
+						sorter = "string";
+						break;
